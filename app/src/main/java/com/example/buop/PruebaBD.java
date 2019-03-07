d1bfcf388450ce6aa5c391cdd530ef2d4447f0b6
Carlos <camartto@users.noreply.github.com>
2019-03-07 17:57:05 +0100

    intento de añadir a la base un usuario

--- app/src/main/java/com/example/buop/PruebaBD.java
+++ app/src/main/java/com/example/buop/PruebaBD.java
@@ -3,32 +3,31 @@ package com.example.buop;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity {
+public class PruebaBD extends AppCompatActivity {
 
     private DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
-    EditText txtUsuario;
-    EditText txtPass;
+    TextView txtUsuario;
+    TextView txtPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prueba_bd);
-        txtUsuario= (EditText) findViewById(R.id.txvUsuario);
-        txtPass= (EditText) findViewById(R.id.txvPassword);
+        txtUsuario= (TextView) findViewById(R.id.txvUsuario);
+        txtPass= (TextView) findViewById(R.id.txvPassword);
     }
 
     private void addUser(String idBD,String user, String pass){
 
-        mDatabase.child("Usuarios").child(idBD).child("Nombre").setValue(user);
-        mDatabase.child("Usuarios").child(idBD).child("Contraseña").setValue(pass);
+        mDatabase.child("usuarios").child(idBD).child("username").setValue(user);
+        mDatabase.child("usuarios").child(idBD).child("password").setValue(pass);
     }
 
     public void EnviarABD(View view) {
-        addUser("45943285", txtUsuario.getText().toString(),txtPass.getText().toString());
+        addUser("45943285",(String) txtUsuario.getText(),(String)txtPass.getText());
     }
 }
---
+++ app/src/main/java/com/example/buop/Usuario.java
@@ -0,0 +1,18 @@
+package com.example.buop;
+
+public class Usuario {
+    private String user;
+    private String contra;
+
+
+
+    public Usuario(String u, String c){
+        this.user=u;
+        this.contra=c;
+
+    }
+    public Usuario(){
+
+
+    }
+}
