5d2883edd45ca30393f6cc8d327c7dc676eb4802
feortic <feortic@unirioja.es>
2019-03-20 11:36:03 +0100

    Añadido la portada

---
+++ app/src/main/java/com/example/buop/portada.java
@@ -0,0 +1,13 @@
+package com.example.buop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class portada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_portada);
+    }
+}
